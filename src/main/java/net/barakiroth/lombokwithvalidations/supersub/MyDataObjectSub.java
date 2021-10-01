55654ee934b14da23ef783444e66480cccbbaec2
ooroor <oyvind@barakiroth.net>
2021-10-01 23:37:21 +0200

    Remove some commented out delombok code.

--- src/main/java/net/barakiroth/lombokwithvalidations/supersub/MyDataObjectSub.java
+++ src/main/java/net/barakiroth/lombokwithvalidations/supersub/MyDataObjectSub.java
@@ -3,7 +3,6 @@ package net.barakiroth.lombokwithvalidations.supersub;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
-import net.barakiroth.lombokwithvalidations.plain.MyDataObject;
 import net.barakiroth.lombokwithvalidations.validation.*;
 
 import java.util.Collection;
@@ -27,7 +26,6 @@ public class MyDataObjectSub extends MyDataObjectSuper {
     public static MyDataObjectSub.MyDataObjectSubBuilder<?, ?> builder(final CategorizedValidationStrategy<MyDataObjectSub>... categorizedValidationStrategies) {
         return MyDataObjectSub.internalBuilder(ValidationStrategyCollector.collect(categorizedValidationStrategies));
     }
-    // AND/OR:
 
     /**
      * All provided strategies will be supplied with an error severity.
@@ -42,18 +40,12 @@ public class MyDataObjectSub extends MyDataObjectSuper {
 
     private static MyDataObjectSub.MyDataObjectSubBuilder<?, ?> internalBuilder(final Set<CategorizedValidationStrategy<MyDataObjectSub>> uniqueCategorizedValidationStrategies) {
 
-        final MyDataObjectSub.MyDataObjectSubBuilderImpl  dataObjectBuilder =
+        final MyDataObjectSub.MyDataObjectSubBuilderImpl dataObjectBuilder =
                 (MyDataObjectSub.MyDataObjectSubBuilderImpl) MyDataObjectSub.internalBuilder();
         dataObjectBuilder.setUniqueCategorizedValidationStrategies(uniqueCategorizedValidationStrategies);
 
         return dataObjectBuilder;
     }
-
-    /*
-    protected MyDataObjectSub(MyDataObjectSubBuilder<?, ?> b) {
-        super(b);
-        this.s = b.s;
-    }*/
 
     public static MyDataObjectSub.MyDataObjectSubBuilder<?, ?> internalBuilder() {
         return new MyDataObjectSubBuilderImpl();
@@ -68,41 +60,8 @@ public class MyDataObjectSub extends MyDataObjectSuper {
         public C build(final Collection<ConstraintViolation<MyDataObjectSub>> constraintViolations) {
             return internalBuild(constraintViolations);
         }
+
         protected abstract C internalBuild(final Collection<ConstraintViolation<MyDataObjectSub>> constraintViolations);
-/*
-        public MyDataObjectSub build(final Collection<ConstraintViolation<MyDataObjectSub>> constraintViolations) {
-
-            // First construct an unvalidated instance for the validator
-            // to be able to get the builder's values because the builder
-            // does not provide getters of its fields.
-            return Validator.validate(internalBuild(), this.uniqueCategorizedValidationStrategies, constraintViolations);
-        }
- */
-
-        /* BEGIN DELOPMBOK */
-        /*
-        private String s;
-
-        public B withS(String s) {
-            this.s = s;
-            return self();
-        }
-
-         */
-
-        /* BEGIN DELOPMBOK */
-        /*
-        protected abstract B self();
-
-        public abstract C internalBuild();
-
-        public String toString() {
-            return "MyDataObjectSub.MyDataObjectSubBuilder(super=" + super.toString() + ", s=" + this.s + ")";
-        }
-        */
-        /* END DELOMBOK */
-        /**/
-        /* END DELOMBOK */
     }
 
     private static final class MyDataObjectSubBuilderImpl extends MyDataObjectSubBuilder<MyDataObjectSub, MyDataObjectSubBuilderImpl> {
@@ -113,18 +72,6 @@ public class MyDataObjectSub extends MyDataObjectSuper {
             this.uniqueCategorizedValidationStrategies = uniqueCategorizedValidationStrategies;
         }
 
-
-
-        /* BEGIN DELOPMBOK */
-        /**/
-        /*
-        private MyDataObjectSubBuilderImpl() {
-        }
-        protected MyDataObjectSubBuilderImpl self() {
-            return this;
-        }
-*/
-/**/
         public MyDataObjectSub internalBuild() {
             return internalBuild(null);
         }
@@ -132,7 +79,5 @@ public class MyDataObjectSub extends MyDataObjectSuper {
         public MyDataObjectSub internalBuild(final Collection<ConstraintViolation<MyDataObjectSub>> constraintViolations) {
             return Validator.validate(new MyDataObjectSub(this), this.uniqueCategorizedValidationStrategies, constraintViolations);
         }
-         /**/
-        /* END DELOMBOK */
     }
 }
